053cc7102df4e241e14af730f13e2bf5eadbfee7
GabrielRossi01 <rossi17006@gmail.com>
2025-10-16 08:29:39 -0300

    Criando projeto Spring com dependências necessárias e implementando pacote models.

---
+++ src/main/java/br/com/fiap/gamefinder/model/Game.java
@@ -0,0 +1,38 @@
+package br.com.fiap.gamefinder.model;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Game {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    private String title;
+
+    private String description;
+
+    private LocalDate releaseDate;
+
+    private Double rating;
+
+    private Genre genre;
+
+    private Platform platform;
+
+    private String coverUrl;
+
+    private String backdropUrl;
+
+    private boolean inWishlist;
+}
